ff73077c23c5185249522e23be2cf4a5820246e5
stream-toma <toma@Toma-Mac.local>
2020-04-19 10:54:47 +0200

    2.3. Introduction to Vue Test Utils

---
+++ src/components/__tests__/Item.spec.js
@@ -0,0 +1,10 @@
+import Item from '../Item.vue'
+import Vue from 'vue'
+
+describe('Item.vue', () => {
+  test('renders "item"', () => {
+    const Ctor = Vue.extend(Item)
+    const vm = new Ctor().$mount()
+    expect(vm.$el.textContent).toContain('item')
+  })
+})
